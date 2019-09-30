2141f724fea5a62e0f7e5c943957d7e5affa5324
ochowo <ochowoikongbeh@gmail.com>
2019-09-30 12:56:22 +0200

    chore(setup):Setup empty react project setup webpack conffiguration [finishes #168825568]

---
+++ webpack.common.js
@@ -0,0 +1,45 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+const miniCssPlugin = new MiniCssExtractPlugin({
+  filename: '[name].hash.css',
+  chunkFilename: '[id].hash.css',
+});
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, use: 'babel-loader' },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      { test: /\.s(a|c)ss$/, use: ['style-loader', 'css-loader', 'sass-loader'] },
+      { test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8000, // Convert images < 8kb to base64 strings
+              name: 'images/[hash]-[name].[ext]',
+            },
+          },
+        ] },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.jsx'],
+  },
+  plugins: [
+    miniCssPlugin,
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
